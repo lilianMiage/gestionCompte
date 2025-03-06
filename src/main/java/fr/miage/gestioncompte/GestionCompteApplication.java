a84ff32f943487cadebf0b54c97d545f6e067f1a
Lilianroux <lilianroux02@gmail.com>
2025-03-06 14:57:33 +0100

    Start project

---
+++ src/main/java/fr/miage/gestioncompte/GestionCompteApplication.java
@@ -0,0 +1,13 @@
+package fr.miage.gestioncompte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionCompteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionCompteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/miage/gestioncompte/GestionCompteApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.miage.gestioncompte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionCompteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
